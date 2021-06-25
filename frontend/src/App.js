5fd98ba414432cbe5b764560ab0569c64385edb2
Ratnadeep <ratna9429@outlook.com>
2021-06-25 10:55:59 +0530

    Model Set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { BrowserRouter as Router , Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello Ratnadeep</h1>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path = '/'  component = {HomeScreen} exact />
+          <Route path = '/product/:id'  component = {ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
